66f62790989c0fe3e585b8dab39871da9e25c330
猫猫 <2124372338@qq.com>
2026-05-07 00:19:18 +0800

    Add files via upload

---
+++ src/main/java/com/yourname/zerog/ZeroGMod.java
@@ -0,0 +1,47 @@
+package com.yourname.zerog;
+
+import com.yourname.zerog.capability.ZeroGStateProvider;
+import com.yourname.zerog.command.ZeroGCommand;
+import com.yourname.zerog.network.ModNetwork;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.AttachCapabilitiesEvent;
+import net.minecraftforge.event.RegisterCommandsEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod(ZeroGMod.MOD_ID)
+public class ZeroGMod {
+
+    public static final String MOD_ID = "zerog";
+    public static final Logger LOGGER = LogManager.getLogger();
+    public static final PlayerState CLIENT_STATE = new PlayerState();
+
+    public ZeroGMod() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        MinecraftForge.EVENT_BUS.register(this);
+        ModNetwork.register();
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {
+        LOGGER.info("ZeroG Mod initialized!");
+    }
+
+    @SubscribeEvent
+    public void onAttachCapabilities(AttachCapabilitiesEvent<Entity> event) {
+        if (event.getObject() instanceof Player) {
+            event.addCapability(new ResourceLocation(MOD_ID, "zerog_state"), new ZeroGStateProvider());
+        }
+    }
+
+    @SubscribeEvent
+    public void onRegisterCommands(RegisterCommandsEvent event) {
+        ZeroGCommand.register(event.getDispatcher());
+    }
+}
