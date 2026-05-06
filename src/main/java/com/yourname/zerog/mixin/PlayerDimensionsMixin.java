2a58734fc6182878c22eabf46d321ff8ae20c9bc
猫猫 <2124372338@qq.com>
2026-05-07 00:39:23 +0800

    Add files via upload

--- src/main/java/com/yourname/zerog/mixin/PlayerDimensionsMixin.java
+++ src/main/java/com/yourname/zerog/mixin/PlayerDimensionsMixin.java
@@ -12,12 +12,12 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Player.class)
 public abstract class PlayerDimensionsMixin {
-    @Inject(method = "m_6972_", at = @At("RETURN"), cancellable = true)
+    @Inject(method = "getDimensions", at = @At("RETURN"), cancellable = true)
     private void zerog$onGetDimensions(Pose pose, CallbackInfoReturnable<EntityDimensions> cir) {
         PlayerState state;
         Player player = (Player) (Object) this;
-        if (player.m_9236_().m_5776_() && (state = ZeroGMod.CLIENT_STATE) != null && state.isZeroGEnabled) {
-            cir.setReturnValue(EntityDimensions.m_20395_(0.6f, 1.8f));
+        if (player.level().isClientSide() && (state = ZeroGMod.CLIENT_STATE) != null && state.isZeroGEnabled) {
+            cir.setReturnValue(EntityDimensions.scalable(0.6f, 1.8f));
         }
     }
 }
--- src/main/java/com/yourname/zerog/mixin/PlayerPositionMixin.java
+++ src/main/java/com/yourname/zerog/mixin/PlayerPositionMixin.java
@@ -14,21 +14,20 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public abstract class PlayerPositionMixin {
     private static final float HALF_HEIGHT = 0.9f;
 
-    @Inject(method = "m_6089_", at = @At("RETURN"), cancellable = true)
+    @Inject(method = "getBoundingBox", at = @At("RETURN"), cancellable = true)
     private void zerog$onMakeBoundingBox(CallbackInfoReturnable<AABB> cir) {
         PlayerState state;
         AABB originalBox;
         Player player = (Player) (Object) this;
-        if (player.m_9236_().m_5776_() && (state = ZeroGMod.CLIENT_STATE) != null && state.isZeroGEnabled && state.orientationInitialized && (originalBox = cir.getReturnValue()) != null) {
+        if (player.level().isClientSide() && (state = ZeroGMod.CLIENT_STATE) != null && state.isZeroGEnabled && state.orientationInitialized && (originalBox = cir.getReturnValue()) != null) {
             Vector3f centerOffset = new Vector3f(0.0f, HALF_HEIGHT, 0.0f);
             state.orientation.transform(centerOffset);
             double offsetX = centerOffset.x;
             double offsetY = centerOffset.y - HALF_HEIGHT;
             double offsetZ = centerOffset.z;
-            if (Math.abs(offsetX) < 0.01d && Math.abs(offsetY) < 0.01d && Math.abs(offsetZ) < 0.01d) {
+            if (Math.abs(offsetX) < 0.01 && Math.abs(offsetY) < 0.01 && Math.abs(offsetZ) < 0.01)
                 return;
-            }
-            AABB adjustedBox = originalBox.m_82386_(offsetX, offsetY, offsetZ);
+            AABB adjustedBox = originalBox.move(offsetX, offsetY, offsetZ);
             cir.setReturnValue(adjustedBox);
         }
     }
