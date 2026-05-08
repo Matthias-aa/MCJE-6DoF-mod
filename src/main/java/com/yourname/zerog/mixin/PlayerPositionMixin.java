145dc6a72c33aecdff140280a41946b30eb2ca9b
猫猫 <2124372338@qq.com>
2026-05-09 00:21:04 +0800

    Update PlayerPositionMixin.java

--- src/main/java/com/yourname/zerog/mixin/PlayerPositionMixin.java
+++ src/main/java/com/yourname/zerog/mixin/PlayerPositionMixin.java
@@ -1,26 +1,10 @@
-package com.yourname.zerog.mixin;
-
-import com.yourname.zerog.ZeroGMod;
-import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.phys.AABB;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-
 @Mixin(Entity.class)
 public abstract class PlayerPositionMixin {
     @Inject(method = "m_20191_", at = @At("RETURN"), cancellable = true, remap = false)
     private void zerog$onGetBoundingBox(CallbackInfoReturnable<AABB> cir) {
         if ((Object)this instanceof Player player) {
-            if (player.level().isClientSide && ZeroGMod.CLIENT_STATE.isZeroGEnabled) {
-                // 维持稳定的碰撞箱
-                AABB box = cir.getReturnValue();
-                if (box != null) {
-                    cir.setReturnValue(box); 
-                }
-            }
+             // 如果在零重力下，可以返回一个以玩家为中心的正方体 AABB
+             // 这能解决侧着身子进不去门或者卡墙的问题
         }
     }
 }
